70681aa2cf92bc9d47d6e2075a8f95a333a5c535
Dexter1300 <127375@student.uni.opole.pl>
2020-11-18 13:21:18 +0100

    Dodanie nowej klasy i serializacja listy

--- Studenci_lista4/MainWindow.xaml.cs
+++ Studenci_lista4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Studenci_lista4
 {
@@ -20,9 +23,63 @@ namespace Studenci_lista4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Person> m_oPersonList = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitBinding();
+        }
+
+        private void InitBinding()
+        {
+            m_oPersonList = new List<Person>();
+            try
+            {
+                using (var reader = new StreamReader("PersonList.xml"))
+                {
+                    XmlSerializer deserializer = new XmlSerializer(typeof(List<Person>),
+                        new XmlRootAttribute("ArrayOfPerson"));
+                    m_oPersonList = (List<Person>)deserializer.Deserialize(reader);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Brak pliku do załadowania!", "Uwaga", MessageBoxButton.OK);
+            }
+
+            if (m_oPersonList.Count == 0)
+            {
+                m_oPersonList.Add(new Person(1, "Jan", "Kowalski", 25, 9909090));
+                m_oPersonList.Add(new Person(2, "Adam", "Nowak", 24, 899898030));
+                m_oPersonList.Add(new Person(3, "Agnieszka", "Kowalczyk", 20, 0032329309));
+            }
+            lstPersons.ItemsSource = m_oPersonList;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            win2 window2 = new win2();
+            window2.Show();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Window3 win3 = new Window3();
+            win3.Show();
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            InitBinding();
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            var serializer = new XmlSerializer(m_oPersonList.GetType());
+            using (var writer = XmlWriter.Create("PersonList.xml"))
+            {
+                serializer.Serialize(writer, m_oPersonList);
+            }
         }
     }
 }
---
+++ Studenci_lista4/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Studenci_lista4
+{
+    public class Person
+    {
+        [XmlAttribute("id")]
+        public int PersonId { get; set; }
+
+        [XmlAttribute("imie")]
+        public string FirstName { get; set; }
+
+        [XmlAttribute("nazwisko")]
+        public string LastName { get; set; }
+
+        [XmlAttribute("wiek")]
+        public int Age { get; set; }
+
+        [XmlAttribute("pesel")]
+        public long Pesel { get; set; }
+
+        public Person(int nPersonId, string sFirstName, string sLastName, int nAge, long lPesel)
+        {
+            PersonId = nPersonId;
+            FirstName = sFirstName;
+            LastName = sLastName;
+            Age = nAge;
+            Pesel = lPesel;
+        }
+
+        public Person()
+        {
+            PersonId = 0;
+            FirstName = "Janusz";
+            LastName = "Januszewski";
+            Age = 120;
+            Pesel = 999909090;
+        }
+    }
+}
